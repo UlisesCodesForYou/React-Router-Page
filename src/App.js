4fd48112e38858b4829ef518550f421304beb95e
UlisesCodesForYou <76757797+UlisesCodesForYou@users.noreply.github.com>
2022-03-21 10:20:15 -0700

    Added switch component, exact prop, and redirect.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 import Welcome from "./Pages/Welcome";
 import Products from "./Pages/Products";
@@ -10,15 +10,20 @@ function App() {
     <div>
       <MainHeader></MainHeader>
       <main>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/products">
-          <Products />
-        </Route>
-        <Route path="/products/:productId">
-          <ProductDetails />
-        </Route>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetails />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -19,4 +19,6 @@ const Products = () => {
   );
 };
 
+
+
 export default Products;
