2b3e6eea22c93b474fce020b7830971b5bbba4be
UlisesCodesForYou <76757797+UlisesCodesForYou@users.noreply.github.com>
2022-03-20 19:24:55 -0700

    Added basic Product paths

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { Route } from "react-router-dom";
+
+import Welcome from "./Pages/Welcome";
+import Products from "./Pages/Products";
+import MainHeader from "./Components/MainHeader";
+import ProductDetails from "./Pages/ProductDetails";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <MainHeader></MainHeader>
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+        <Route path="/product-details/:productId">
+          <ProductDetails />
+        </Route>
+      </main>
     </div>
   );
 }
---
+++ src/Pages/Products.js
@@ -0,0 +1,16 @@
+const Products = () => {
+  return (
+    <section>
+      <h1>These are the products</h1>
+      <ul>
+        <li>
+          <h2>Beige chino </h2>
+          <h2>Demin jacket</h2>
+          <h2>Vintage leather thong?</h2>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
